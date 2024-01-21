1b7152cb4dcd35952dbac1ab4983033e407d7727
fikrialwan <fikriar93@gmail.com>
2024-01-21 20:52:53 +0700

    feat: add initial custom page

---
+++ src/app/(qr-layout)/custom/page.tsx
@@ -0,0 +1,7 @@
+export default function Custom() {
+  return (
+    <div>
+      <h1>Custom page</h1>
+    </div>
+  );
+}
--- src/app/(qr-layout)/layout.tsx
+++ src/app/(qr-layout)/layout.tsx
@@ -8,11 +8,11 @@ export default function QrLayout({
   return (
     <div className="h-screen w-full max-w-5xl m-auto">
       <header>
-        <nav className="py-6">
+        <nav className="py-6 px-4">
           <Link href="/">&lt;- Back</Link>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className="px-4">{children}</main>
     </div>
   );
 }
