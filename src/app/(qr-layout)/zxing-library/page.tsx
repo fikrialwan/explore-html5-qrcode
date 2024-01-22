f53c124afb25bddfd33e8a94d61a2cf074fd6902
fikrialwan <fikriar93@gmail.com>
2024-01-22 14:13:22 +0700

    chore: add zxing/library

---
+++ src/app/(qr-layout)/zxing-library/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { BrowserMultiFormatReader } from "@zxing/library";
+import { useRouter } from "next/navigation";
+
+export default function ZxingLibrary() {
+    const router = useRouter()
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const reader = useRef(new BrowserMultiFormatReader());
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+    reader.current.decodeFromConstraints(
+      {
+        audio: false,
+        video: {
+          facingMode: "environment",
+        },
+      },
+      videoRef.current,
+      (result, error) => {
+        if (result) router.replace("/?result=" + result?.getText());
+        if (error) console.log(error);
+      }
+    );
+    return () => {
+      reader.current.reset();
+    };
+  }, [videoRef]);
+
+  return <video ref={videoRef} className="h-full w-full" />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home({
   const { result } = searchParams;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav className="mb-32 flex flex-row text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <nav className="flex flex-col text-center lg:max-w-5xl w-full lg:mb-0 lg:grid-cols-4 lg:text-left ">
         <Link
           href="/default"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -50,6 +50,20 @@ export default function Home({
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Using react-qr-reader
+          </p>
+        </Link>
+        <Link
+          href="/zxing-library"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Zxing Library{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Using zxing/library
           </p>
         </Link>
       </nav>
