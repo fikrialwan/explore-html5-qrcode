6f542114265a20a7bff28e7c3bb7c407aba71c5d
fikrialwan <fikriar93@gmail.com>
2024-01-22 14:50:35 +0700

    fix: using state for size

--- src/app/(qr-layout)/zxing-library/page.tsx
+++ src/app/(qr-layout)/zxing-library/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BrowserMultiFormatReader } from "@zxing/library";
 import { useRouter } from "next/navigation";
 
@@ -9,8 +9,10 @@ export default function ZxingLibrary() {
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const reader = useRef(new BrowserMultiFormatReader());
+  const [size, setSize] = useState({heigth: 0, width: 0})
 
   useEffect(() => {
+    setSize({heigth: screen.availHeight, width: screen.availWidth})
     if (!videoRef.current) return;
     reader.current.decodeFromConstraints(
       {
@@ -35,7 +37,7 @@ export default function ZxingLibrary() {
     <>
       <video ref={videoRef} className="h-full w-full" />
       <p>
-        width: {screen.availHeight} <br /> height: {screen.availHeight}
+        width: {size.width} <br /> height: {size.heigth}
       </p>
     </>
   );
