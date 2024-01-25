52f02276ad241d91e4dfe63b1eab3f03f1796521
fikrialwan <fikriar93@gmail.com>
2024-01-25 16:49:10 +0700

    feat: remove static heigth style

--- src/app/(qr-layout)/zxing-library/page.tsx
+++ src/app/(qr-layout)/zxing-library/page.tsx
@@ -44,6 +44,9 @@ export default function ZxingLibrary() {
   }, [videoRef]);
 
   return (
-    <video ref={videoRef} className="max-h-32 w-full" />
+    <div className="h-36 w-full relative">
+      <video ref={videoRef} className="h-full w-full" />
+      <div className="w-72 h-28 border-2 border-red-400 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+    </div>
   );
 }
