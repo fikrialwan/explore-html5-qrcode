deeb7218b5289971d55d3d46be9098486fad175e
fikrialwan <fikriar93@gmail.com>
2024-01-25 16:27:05 +0700

    feat: add style to scope scan

--- src/app/(qr-layout)/zxing-library/page.tsx
+++ src/app/(qr-layout)/zxing-library/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { BrowserMultiFormatReader, BarcodeFormat, DecodeHintType  } from "@zxing/library";
+import {
+  BrowserMultiFormatReader,
+  BarcodeFormat,
+  DecodeHintType,
+} from "@zxing/library";
 import { useRouter } from "next/navigation";
 
 export default function ZxingLibrary() {
@@ -9,7 +13,7 @@ export default function ZxingLibrary() {
 
   const hints = new Map();
   const formats = [BarcodeFormat.CODE_39];
-  
+
   hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -17,7 +21,7 @@ export default function ZxingLibrary() {
 
   useEffect(() => {
     if (!videoRef.current) return;
-    reader.current
+    reader.current;
     reader.current.decodeFromConstraints(
       {
         audio: false,
@@ -25,14 +29,14 @@ export default function ZxingLibrary() {
           width: { min: 640, ideal: 1280, max: 1920 },
           height: { min: 480, ideal: 720, max: 1080 },
           aspectRatio: { min: 1, max: 2, ideal: 1 },
-          facingMode: "environment"
+          facingMode: "environment",
         },
       },
       videoRef.current,
       (result) => {
         if (result) router.replace("/?result=" + result?.getText());
       }
-    )
+    );
 
     return () => {
       reader.current.reset();
@@ -40,8 +44,9 @@ export default function ZxingLibrary() {
   }, [videoRef]);
 
   return (
-    <>
+    <div className="h-full w-full relative">
       <video ref={videoRef} className="h-full w-full" />
-    </>
+      <div className="w-72 h-28 border-2 border-red-400 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+    </div>
   );
 }
