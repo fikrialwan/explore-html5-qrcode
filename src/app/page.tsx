effb408260b94a7e6559d6c9546b6290dc678766
fikrialwan <fikriar93@gmail.com>
2024-02-05 11:04:30 +0700

    feat: remove dynamsoft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,20 +106,6 @@ export default function Home({
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Using Quagga2</p>
         </Link>
-        <Link
-          href="/dynasoft"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Dynamsoft{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Using Dynamsoft trial 30 days
-          </p>
-        </Link>
       </nav>
       <section>
         <h1>Result</h1>
