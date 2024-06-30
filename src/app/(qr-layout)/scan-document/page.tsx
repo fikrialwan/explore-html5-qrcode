13b3e27971626a01cec55639bf2271c62e5b9c76
fikrialwan <fikriar93@gmail.com>
2024-06-30 14:53:12 +0700

    feat: increase max to 4k

--- src/app/(qr-layout)/scan-document/page.tsx
+++ src/app/(qr-layout)/scan-document/page.tsx
@@ -41,8 +41,8 @@ export default function ScanDocument() {
       .getUserMedia({
         audio: false,
         video: {
-          width: { min: 640, ideal: 1920, max: 1920 },
-          height: { min: 480, ideal: 1080, max: 1080 },
+          width: { min: 640, ideal: 1920, max: 3840 },
+          height: { min: 480, ideal: 1080, max: 2160 },
           aspectRatio: { min: 1, max: 2, ideal: 1 },
           frameRate: { ideal: 60 },
           facingMode: "environment",
