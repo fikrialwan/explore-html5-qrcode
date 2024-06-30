3c435d53c27c1b5594192ae37c3e8320738427e4
fikrialwan <fikriar93@gmail.com>
2024-06-30 13:29:33 +0700

    feat: action button

--- src/app/(qr-layout)/scan-document/page.tsx
+++ src/app/(qr-layout)/scan-document/page.tsx
@@ -5,6 +5,34 @@ import { useEffect, useRef } from "react";
 export default function ScanDocument() {
   const videoRef = useRef<HTMLVideoElement>(null);
 
+  const handleCapture = () => {
+    if (videoRef.current) {
+      const video = videoRef.current;
+      const canvas = document.createElement("canvas");
+      const w = video.videoWidth * 0.8;
+      const h = video.videoWidth * (5 / 8);
+      canvas.width = w;
+      canvas.height = h;
+      canvas
+        .getContext("2d")
+        ?.drawImage(
+          video,
+          (video.videoWidth - w) / 2,
+          video.videoHeight * 0.15,
+          w,
+          h,
+          0,
+          0,
+          w,
+          h
+        );
+      canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob as Blob);
+        window.open(url);
+      });
+    }
+  };
+
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({
@@ -38,9 +66,18 @@ export default function ScanDocument() {
   }, []);
 
   return (
-    <div className="h-full w-full relative bg-black">
-      <video ref={videoRef} className="h-full w-full"></video>
-      <div className="absolute w-full h-auto top-[15px] left-1/2 -translate-x-1/2 max-w-[80%] max-h-[60%] aspect-[8/5] border-white border-2 rounded-md bg-transparent shadow-scan" />
-    </div>
+    <>
+      <video
+        ref={videoRef}
+        className="h-screen w-screen fixed top-0 left-0 z-10"
+      ></video>
+      <div className="fixed w-full h-auto top-[15%] left-1/2 -translate-x-1/2 max-w-[80%] max-h-[60%] aspect-[8/5] border-white border-2 rounded-md bg-transparent shadow-scan z-20" />
+      <button
+        className="fixed w-full py-3 bg-black text-white bottom-0 left-0 z-50"
+        onClick={handleCapture}
+      >
+        Capture
+      </button>
+    </>
   );
 }
