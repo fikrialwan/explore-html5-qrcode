0d440459ce99e1bd1227f973bf39d7628efa5d71
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2024-10-01 22:31:41 +0700

    back to normal resolution

--- src/app/(qr-layout)/zxing-qrcode-library/page.tsx
+++ src/app/(qr-layout)/zxing-qrcode-library/page.tsx
@@ -26,10 +26,8 @@ export default function ZxingLibrary() {
       {
         audio: false,
         video: {
-          // width: { min: 640, ideal: 1280, max: 1920 },
-          // height: { min: 480, ideal: 720, max: 1080 },
-          width: { min: 640, ideal: 1920, max: 3840 },
-          height: { min: 480, ideal: 1080, max: 2160 },
+          width: { min: 640, ideal: 1280, max: 1920 },
+          height: { min: 480, ideal: 720, max: 1080 },
           aspectRatio: { min: 1, max: 2, ideal: 1 },
           facingMode: 'environment',
         },
